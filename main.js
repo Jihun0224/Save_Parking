e52ce2a738afbf6471a27a06d1e1d1e74b2e605e
Kongjihun <wlgns3914@naver.com>
2021-03-12 03:37:14 +0900

    search bar

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import { StyleSheet, View } from 'react-native';
+import { SearchBar } from 'react-native-elements';
 import NaverMapView, {Circle, Marker, Path, Polyline, Polygon} from "react-native-nmap";
+// import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class Main extends Component{
   constructor(){
@@ -13,12 +15,28 @@ export default class Main extends Component{
   searchTextInputChanged(text) {
     this.setState({ searchedText: text })
   }
+  
+//   componentDidMount(){
+//     var queryParams = '?' + encodeURIComponent('ServiceKey') + '=서비스키'; /* Service Key*/
+//         queryParams += '&' + encodeURIComponent('type') + '=' + encodeURIComponent('json'); /*타입*/
+//         queryParams += '&' + encodeURIComponent('institutionNm') + '=' + encodeURIComponent(''); /**/
+
+
+//   } 
 
   render(){
     const currPos = {latitude: this.props.currPos.latitude, longitude: this.props.currPos.longitude};
     return (
       <View style={styles.container}>
             
+            <SearchBar style={styles.textinput} 
+                      platform="android"
+                      onChangeText = {(text) => this.searchTextInputChanged(text)}
+                      containerStyle={{borderWidth: 1, borderRadius: 15}}
+                    //   searchIcon={<Icon name="location" size={24} color="gray"/>}
+                      placeholder='장소를 입력해 주세요.'/>
+           
+
             <View style={styles.map}>
             <NaverMapView style={{width: '100%', height: '100%'}}
                           showsMyLocationButton={true}
@@ -26,6 +44,7 @@ export default class Main extends Component{
                           onTouch={e => console.log('onTouch', JSON.stringify(e.nativeEvent))}
                           onCameraChange={e => console.log('onCameraChange', JSON.stringify(e))}
                           onMapClick={e => console.log('onMapClick', JSON.stringify(e))}>
+
 
             </NaverMapView>
             </View>
