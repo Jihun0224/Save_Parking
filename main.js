c4e02dbf1ca884f420db75132aaec90a159d5d21
Kongjihun <wlgns3914@naver.com>
2021-03-12 03:26:58 +0900

    nmap

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
 import React, {Component} from 'react';
-import { StyleSheet, View,Text } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import NaverMapView, {Circle, Marker, Path, Polyline, Polygon} from "react-native-nmap";
 
 export default class Main extends Component{
+  constructor(){
+    super();
+    this.state={
+        search:'',
+    }
+  }
 
+  searchTextInputChanged(text) {
+    this.setState({ searchedText: text })
+  }
 
   render(){
     const currPos = {latitude: this.props.currPos.latitude, longitude: this.props.currPos.longitude};
     return (
       <View style={styles.container}>
             
-            <Text>{currPos.latitude}</Text>
-            <Text>{currPos.longitude}</Text>
+            <View style={styles.map}>
+            <NaverMapView style={{width: '100%', height: '100%'}}
+                          showsMyLocationButton={true}
+                          center={{...currPos, zoom: 16}}
+                          onTouch={e => console.log('onTouch', JSON.stringify(e.nativeEvent))}
+                          onCameraChange={e => console.log('onCameraChange', JSON.stringify(e))}
+                          onMapClick={e => console.log('onMapClick', JSON.stringify(e))}>
 
+            </NaverMapView>
+            </View>
       </View>
     )}
 }
@@ -20,4 +36,8 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
     },
+    map:{
+      flex:1,
+    }
+
   });
