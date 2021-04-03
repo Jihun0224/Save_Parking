273c008afd5bb238148267a4a4fb07c781ae844a
Kongjihun <wlgns3914@naver.com>
2021-04-03 19:57:58 +0900

    Zoom in&out 버튼 추가

--- src/main.js
+++ src/main.js
@@ -20,8 +20,45 @@ export default class Main extends Component{
       currPos:{
         latitude:this.props.currPos.latitude,
         longitude:this.props.currPos.longitude,
+        latitudeDelta: 0.00522,
+        longitudeDelta: Dimensions.get("window").width / Dimensions.get("window").height * 0.00522
       }
     }
+  }
+  onPressZoomOut() {
+    this.region = {
+      latitude: this.state.currPos.latitude,
+      longitude: this.state.currPos.longitude,
+      latitudeDelta: this.state.currPos.latitudeDelta * 10,
+      longitudeDelta: this.state.currPos.longitudeDelta * 10
+    }
+
+    this.setState({
+      currPos: {
+        latitudeDelta: this.region.latitudeDelta,
+        longitudeDelta: this.region.longitudeDelta,
+        latitude: this.region.latitude,
+        longitude: this.region.longitude
+      }
+    })
+    this.map.animateToRegion(this.region, 100);
+  }
+  onPressZoomIn() {
+    this.region = {
+      latitude: this.state.currPos.latitude,
+      longitude: this.state.currPos.longitude,
+      latitudeDelta: this.state.currPos.latitudeDelta / 10,
+      longitudeDelta: this.state.currPos.longitudeDelta / 10
+    }
+    this.setState({
+      currPos: {
+        latitudeDelta: this.region.latitudeDelta,
+        longitudeDelta: this.region.longitudeDelta,
+        latitude: this.region.latitude,
+        longitude: this.region.longitude
+      }
+    })
+    this.map.animateToRegion(this.region, 100);
   }
   closeSearch(){
     this.setState({isChildVisible:false});
@@ -30,7 +67,9 @@ export default class Main extends Component{
     this.setState({currPos:searchedPlace});
   }
   _onMapReady = () => this.setState({marginBottom: 0}) 
-
+  onRegionChange(region) {
+    this.setState({ currPos:region });
+  }
   render(){
     return (
       <SafeAreaView style={styles.container}>
@@ -57,34 +96,51 @@ export default class Main extends Component{
             />
           </AnimatedHideView>
           {!this.state.isChildVisible&&
-          <View style={StyleSheet.absoluteFillObject}>
-          
-            <MapView
-            style={{flex:1, marginBottom: this.state.marginBottom}}
-            mapPadding={{ top: 100, right: 0, bottom: 0, left: 0 }}
-            onMapReady={this._onMapReady}
-            showsUserLocation={true}
-            provider={PROVIDER_GOOGLE}
-            showsMyLocationButton={true}
-            minPoints={2}
-            minZoom={3}
-            clusterColor={"#002166"}
-            initialRegion={{
-              latitude: this.state.currPos.latitude,
-              longitude: this.state.currPos.longitude,
-              latitudeDelta: 0.00522,
-              longitudeDelta: 0.00421,
-            }}
-          >
-            <Marker title="ddd" coordinate={{ latitude: this.props.currPos.latitude, longitude: this.props.currPos.longitude }}>
+            <View style={StyleSheet.absoluteFillObject}>
+              <MapView
+                initialRegion={this.state.currPos}
+                style={{flex:1, marginBottom: this.state.marginBottom}}
+                mapPadding={{ top: 100, right: 0, bottom: 0, left: 0 }}
+                onMapReady={this._onMapReady}
+                showsUserLocation={true}
+                provider={PROVIDER_GOOGLE}
+                showsMyLocationButton={true}
+                followUserLocation={true}
+                minPoints={2}
+                minZoom={3}
+                clusterColor={"#002166"}
+                zoomEnabled={true}
+                mapRef={ref => this.map = ref}
+                onRegionChange={(initialRegion)=>{this.onRegionChange(initialRegion)}}
+            >
+              <Marker coordinate={{ latitude: this.props.currPos.latitude, longitude: this.props.currPos.longitude }}>
+                <CustomMarker/>
+              </Marker>
+              <Marker coordinate={{ latitude: this.props.currPos.latitude+0.0003, longitude: this.props.currPos.longitude+0.0032 }}>
               <CustomMarker/>
-            </Marker>
-            <Marker coordinate={{ latitude: this.props.currPos.latitude+0.0003, longitude: this.props.currPos.longitude+0.0032 }}>
-            <CustomMarker/>
-            </Marker>
-
-          </MapView>
-          </View>
+              </Marker>
+            </MapView>
+              <TouchableOpacity
+              style={styles.zoomin}
+              onPress={() => { this.onPressZoomIn() }}
+              >
+                <Icon
+                  name="add"
+                  style={{...styles.icon,backgroundColor: 'rgba(255, 255, 255, 0.6)',color: 'rgba(0, 0, 0, 0.8)'}}
+                  size={25}
+                />
+            </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.zoomout}
+                onPress={() => { this.onPressZoomOut() }}
+              >
+                <Icon
+                  name="remove"
+                  style={{...styles.icon,backgroundColor: 'rgba(255, 255, 255, 0.6)',color: 'rgba(0, 0, 0, 0.8)'}}
+                  size={25}
+                />
+              </TouchableOpacity>
+            </View>
           }
 
             <SlidingUpPanel 
@@ -113,7 +169,7 @@ const styles = StyleSheet.create({
       zIndex:1,
     },
     openSearch:{
-      zIndex:1,
+      zIndex:2,
       position:"absolute",
       top:10,
       width:window.width-30,
@@ -133,5 +189,26 @@ const styles = StyleSheet.create({
     search_window:{
       flex:1,
       backgroundColor:"white"
+    },
+    icon:{
+      width:35,
+      height:35,
+      paddingLeft:4,
+      paddingTop:4
+    },
+    zoomin:{
+      position:'absolute',
+      top:300,
+      zIndex:2,
+      left:window.width-50,
+      alignSelf:'center'
+
+    },
+    zoomout:{
+      position:'absolute',
+      top:335,
+      zIndex:2,
+      left:window.width-50
+
     }
   });
