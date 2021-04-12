f18e5d536fed080c88eb91706382a43833c2c85c
ymsong96 <ymsong96@naver.com>
2021-04-12 14:19:56 +0900

    마커 부분 수정

---
+++ src/cctvMarker.js
@@ -0,0 +1,43 @@
+import React,{Component} from 'react';
+import { StyleSheet, ImageBackground,Text,View } from 'react-native';
+import CCTV from './cctv.png';
+import Marker from './marker.png';
+
+export default class cctvMarker extends Component{
+    render(){
+        return (
+        <View  style={styles.marker}>
+          <ImageBackground
+                resizeMode="contain"
+                source={Marker}
+                style={styles.imageBackground}
+                imageStyle={{tintColor:"#002166"}}>
+            <ImageBackground
+                resizeMode="contain"
+                source={CCTV}
+                style={styles.CCTV}
+                imageStyle={{tintColor:"#ffffff"}}>
+            </ImageBackground>
+          </ImageBackground>
+      </View>
+
+    )
+  }
+}
+  const styles = StyleSheet.create({
+    imageBackground: {
+        width:40,
+        height:40,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    CCTV: {
+      width: 30,
+      height: 30,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    marker:{
+        paddingBottom:5
+    }
+  });
